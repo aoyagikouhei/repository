39205953685bb214a40f6027410458929eb5bc20
aoyagikouhei <aoyagi.kouhei@gmail.com>
2015-08-19 16:48:21 +0900

    modify temp project_id

--- app/models/temp.rb
+++ app/models/temp.rb
@@ -2,8 +2,8 @@ class Temp < ActiveRecord::Base
   include ModelUtil
 
   class << self
-    def find_for_available
-      where_for_null_at.order('temp_kbn ASC')
+    def find_for_available(project_id)
+      where_for_null_at.where(project_id: project_id).order('temp_kbn ASC')
     end
 
     def find_for_output(value)
