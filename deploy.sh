c991d9997f1e34ce1d95861e4bbca5d7d671d804
Kosuke Ohmura <ohmura_kosuke@uniquevision.co.jp>
2018-05-14 19:20:50 +0900

    fix failing deploy script

--- deploy.sh
+++ deploy.sh
@@ -6,6 +6,9 @@ DEST_HOST=$1
 rsync -avz --delete --chmod=Da+xr,Du+w,Fa+r,Fu+w --exclude-from="$EXCLUDE" "$BASE_DIR" "$DEST_HOST":"$DEST_DIR"
 ssh "$DST_HOST" bash -ls <<EOT
 [ -f ~/.bash_profile ] && . ~/.bash_profile || . ~/.profile
-export SECRET_KEY_BASE=`bundle exec rake secret`
-cd "$DEST_DIR" && bundle install --path=vendor/bundler && bundle exec rake assets:precompile && bundle exec thin restart -C thin.yaml
+cd "$DEST_DIR" && \
+  export SECRET_KEY_BASE=`bundle exec rake secret` && \
+  bundle install --path=vendor/bundler && \
+  bundle exec rake assets:precompile && \
+  bundle exec thin restart -C thin.yaml
 EOT
